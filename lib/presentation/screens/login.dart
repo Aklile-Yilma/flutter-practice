e78fe173f8f30d10bbb088cb0fe1fba1018cf471
aklile-yilma <aklileyilma@gmail.com>
2023-05-08 14:33:16 +0300

    fix merge conflict

---
+++ lib/presentation/screens/login.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {},
+        ),
+        title: const Text("Welcome Back!"),
+        centerTitle: true,
+      ),
+      body: Column(children: []),
+    );
+  }
+}
